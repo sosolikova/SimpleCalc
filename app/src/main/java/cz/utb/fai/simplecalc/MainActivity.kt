d64fe8a34d49ed5afd71421f1744e2a6a0e20a13
Veronika Sosolikova <v_sosolikova@utb.cz>
2024-02-05 22:56:05 +0100

    onCreate method

--- app/src/main/java/cz/utb/fai/simplecalc/MainActivity.kt
+++ app/src/main/java/cz/utb/fai/simplecalc/MainActivity.kt
@@ -2,10 +2,24 @@ package cz.utb.fai.simplecalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var etNumber1: EditText
+    private lateinit var etNumber2: EditText
+    private lateinit var spinnerOperation: Spinner
+    private lateinit var labelResult: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etNumber1 = findViewById(R.id.etNumber1)
+        etNumber2 = findViewById(R.id.etNumber2)
+        spinnerOperation = findViewById(R.id.spinnerOperation)
+        labelResult = findViewById(R.id.labelResult)
+
+        val operatorsArray = resources.getStringArray(R.array.operators)
     }
 }
