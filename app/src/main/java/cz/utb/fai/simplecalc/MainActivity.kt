dcaa9cddb32848b102a250801287fb28f126079d
Veronika Sosolikova <v_sosolikova@utb.cz>
2024-02-05 23:20:13 +0100

    method calculate created

--- app/src/main/java/cz/utb/fai/simplecalc/MainActivity.kt
+++ app/src/main/java/cz/utb/fai/simplecalc/MainActivity.kt
@@ -2,10 +2,12 @@ package cz.utb.fai.simplecalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var etNumber1: EditText
@@ -26,5 +28,34 @@ class MainActivity : AppCompatActivity() {
         val dataAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, list)
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinnerOperation.adapter = dataAdapter
+    }
+
+    fun calculate(view: View) {
+        val number1: Double
+        val number2: Double
+        var result = 0.0
+
+        try {
+            number1 = etNumber1.text.toString().toDouble()
+            number2 = etNumber2.text.toString().toDouble()
+
+            if (spinnerOperation.selectedItem.equals("+")) {
+                result = number1 + number2
+            } else if (spinnerOperation.selectedItem.equals("-")) {
+                result = number1 - number2
+            } else if (spinnerOperation.selectedItem.equals("*")) {
+                result = number1 * number2
+            } else if (spinnerOperation.selectedItem.equals("/")) {
+                if (number2 == 0.toDouble()) {
+                    labelResult.text = "NULOU NELZE DĚLIT"
+                    return;
+                }
+                result = number1 / number2;
+            }
+            val nf = DecimalFormat("#.##")
+            labelResult.text = nf.format(result)
+        } catch (e: java.lang.NumberFormatException) {
+            labelResult.text = "Chybné zadání"
+        }
     }
 }
